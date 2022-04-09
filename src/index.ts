935be3744a70f58c4bb14dc941cfe7c932c0988c
Marcos Jesus Santana Ramos <alu0101033471@ull.edu.es>
2022-04-09 11:55:26 +0000

    Primeros pasos con inquirer.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import inquirer from 'inquirer';
 import { Album } from "./clases/album";
 import { Artistas } from "./clases/artistas";
 import { Cancion } from "./clases/cancion";
@@ -211,15 +212,145 @@ const playlist3 = new Playlist(`Flamencos de corazóh`, [cancion11, cancion24, c
 
 // Llamadas a métodos
 //console.log(cancion1.getGeneroMusical());
-cancion1.setGeneroMusical([Pop]);
 //console.log(Pop.getGrupos());
 //console.log(Rock);
 //console.log(cancion1.getGeneroMusical());
 //console.log(cancion1);
 //console.log(artista1);
 //console.log(grupo1.dame());
+function print(){
+cancion1.setGeneroMusical([Pop]);
 
 console.log(cancion3.getDuracionCancionSecs());
 console.log(playlist1.getDuracion());
-
-
+}
+enum Commands {
+  Add = `Añadir`,
+  Borrar = `Borrar`,
+  Modificar = `Modificar`,
+  Salir = `Salir`
+}
+enum CommandsClases {
+  Cancion = `Canción`,
+  GeneroMusical = `Género musical`,
+  Artista = `Artista`,
+  Grupo = `Grupo`,
+  Album = `Album`
+}
+async function add(){
+  console.clear();
+  const respuestaadd = await inquirer.prompt({
+    type: 'input',
+    name: `add`,
+    message: `que quieres añadir?`
+  })
+  if (respuestaadd['add'] !== ``) {
+    console.clear();
+    console.log(cancion19.getAutorCancion());
+  }
+  menuPrincipal();
+}
+async function menuAdd(){
+  const respuestaAdd = await inquirer.prompt({
+    type: 'list',
+    name: `command`, 
+    message: `Elige que quieres añadir`,
+    choices: Object.values(CommandsClases)
+  })
+  switch(respuestaAdd["command"]) {
+    case CommandsClases.Cancion:
+     console.log(`añadiendo una cancion`);
+      break;
+    case CommandsClases.GeneroMusical:
+      console.log(`añadiendo una genero musical`);
+      break;
+    case CommandsClases.Album:
+      console.log(`añadiendo una album`);
+      break;
+    case CommandsClases.Artista:
+      console.log(`añadiendo una artista`);
+      break;
+    case CommandsClases.Grupo:
+      console.log(`añadiendo una grupo`);
+      break;
+  }
+  menuPrincipal();
+}
+async function menuDel(){
+  const respuestaDel = await inquirer.prompt({
+    type: 'list',
+    name: `command`, 
+    message: `Elige que quieres eliminar`,
+    choices: Object.values(CommandsClases)
+  })
+  switch(respuestaDel["command"]) {
+    case CommandsClases.Cancion:
+     console.log(`eliminndo una cancion`);
+      break;
+    case CommandsClases.GeneroMusical:
+      console.log(`eliminndo una genero musical`);
+      break;
+    case CommandsClases.Album:
+      console.log(`eliminndo una album`);
+      break;
+    case CommandsClases.Artista:
+      console.log(`eliminndo una artista`);
+      break;
+    case CommandsClases.Grupo:
+      console.log(`eliminndo una grupo`);
+      break;
+  }
+  menuPrincipal();
+}
+async function menuMod(){
+  const respuestaMod = await inquirer.prompt({
+    type: 'list',
+    name: `command`, 
+    message: `Elige que quieres modificar`,
+    choices: Object.values(CommandsClases)
+  })
+  switch(respuestaMod["command"]) {
+    case CommandsClases.Cancion:
+     console.log(`modificando una cancion`);
+      break;
+    case CommandsClases.GeneroMusical:
+      console.log(`modificando una genero musical`);
+      break;
+    case CommandsClases.Album:
+      console.log(`modificando una album`);
+      break;
+    case CommandsClases.Artista:
+      console.log(`modificando una artista`);
+      break;
+    case CommandsClases.Grupo:
+      console.log(`modificando una grupo`);
+      break;
+  }
+  menuPrincipal();
+}
+async function menuPrincipal(){
+  //console.clear();
+  //print();
+  const respuesta = await inquirer.prompt({
+    type: 'list',
+    name: `command`, 
+    message: `Elige opción`,
+    choices: Object.values(Commands)
+  })
+  switch(respuesta["command"]) {
+    case Commands.Add:
+      menuAdd();
+      break;
+    case Commands.Borrar:
+      menuDel();
+      break;
+    case Commands.Modificar:
+      menuMod();
+      break;
+    case Commands.Salir:
+     //process.exit();:;
+      return;
+  }
+  //console.log(respuesta);
+}
+menuPrincipal();
